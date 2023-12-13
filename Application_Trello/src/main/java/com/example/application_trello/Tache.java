712f28e7c28b88ff9747a760a04b3ed699f997ca
Titoufeur <titouan.letondal@gmail.com>
2023-12-13 17:55:06 +0100

    JDK 19

--- Application_Trello/src/main/java/com/example/application_trello/Tache.java
+++ Application_Trello/src/main/java/com/example/application_trello/Tache.java
@@ -1,9 +1,12 @@
 package com.example.application_trello;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Tache {
     protected String nomTache;
     protected String commentaire;
     protected Date dateDebut;
     protected Date dateFin;
-    protected ArrayList<Tache>
+    protected ArrayList<Tache> listeDependances;
 }
