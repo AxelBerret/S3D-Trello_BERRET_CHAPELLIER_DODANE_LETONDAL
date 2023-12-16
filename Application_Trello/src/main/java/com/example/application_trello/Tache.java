04f2bf3f60b9f162a12bb68115f257d8657ba0c4
Titoufeur <titouan.letondal@gmail.com>
2023-12-16 18:08:23 +0100

    Finalisation classe Tâche et TâcheSimple

--- Application_Trello/src/main/java/com/example/application_trello/Tache.java
+++ Application_Trello/src/main/java/com/example/application_trello/Tache.java
@@ -2,11 +2,52 @@ package com.example.application_trello;
 
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.Objects;
 public class Tache {
     protected String nomTache;
     protected String commentaire;
     protected Date dateDebut;
     protected Date dateFin;
     protected ArrayList<Tache> listeDependances;
+
+    public Tache(String pNom){
+        this.nomTache = pNom;
+    }
+
+    public void setCommentaire(String com){
+        this.commentaire = com;
+    }
+
+    public void setDateDebut(Date dateD){
+        this.dateDebut = dateD;
+    }
+
+    public void setDateFin(Date dateF){
+        this.dateFin = dateF;
+    }
+
+     public void ajouterDependance(Tache tache){
+        this.listeDependances.add(tache);
+     }
+
+     public void retirerDependance(Tache tache){
+        this.listeDependances.remove(tache);
+     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Tache autreTache = (Tache) obj;
+        return Objects.equals(nomTache, autreTache.nomTache) &&
+                Objects.equals(commentaire, autreTache.commentaire) &&
+                Objects.equals(dateDebut, autreTache.dateDebut) &&
+                Objects.equals(dateFin, autreTache.dateFin) &&
+                Objects.equals(listeDependances, autreTache.listeDependances);
+    }
+
 }
--- Application_Trello/src/main/java/com/example/application_trello/TacheSimple.java
+++ Application_Trello/src/main/java/com/example/application_trello/TacheSimple.java
@@ -1,4 +1,21 @@
 package com.example.application_trello;
 
-public class TacheSimple {
+import java.util.Date;
+
+public class TacheSimple extends Tache {
+
+    public TacheSimple(String nomTache) {
+        super(nomTache);
+    }
+
+    @Override
+    public String toString() {
+        return "TacheSimple{" +
+                "nomTache='" + nomTache + '\'' +
+                ", commentaire='" + commentaire + '\'' +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", listeDependances=" + listeDependances +
+                '}';
+    }
 }
