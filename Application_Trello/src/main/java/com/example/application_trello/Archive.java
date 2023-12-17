bff080e4ee8dbebb79f7ba5b889a259892f9a66a
logan <logan.chapellier3@etu.univ-lorraine.fr>
2023-12-17 16:07:23 +0100

    écriture de la classe Archive

--- Application_Trello/src/main/java/com/example/application_trello/Archive.java
+++ Application_Trello/src/main/java/com/example/application_trello/Archive.java
@@ -1,4 +1,37 @@
 package com.example.application_trello;
 
+import java.util.ArrayList;
+
 public class Archive {
+    private ArrayList<Tache> listeTachesArchivees;
+    private ArrayList<Colonne> listeColonnesArchivees;
+
+    public Archive(){
+        this.listeTachesArchivees = new ArrayList<Tache>();
+        this.listeColonnesArchivees = new ArrayList<Colonne>();
+    }
+
+    public void archiverTache(Tache tache){
+        this.listeTachesArchivees.add(tache);
+    }
+
+    public void archiverColonne(Colonne colonne){
+        this.listeColonnesArchivees.add(colonne);
+    }
+
+    public void desarchiverTache(Tache tache){
+        this.listeTachesArchivees.remove(tache);
+    }
+
+    public void desarchiverColonne(Colonne colonne){
+        this.listeColonnesArchivees.remove(colonne);
+    }
+
+    public ArrayList<Tache> getListeTachesArchivees(){
+        return this.listeTachesArchivees;
+    }
+
+    public ArrayList<Colonne> getListeColonnesArchivees(){
+        return this.listeColonnesArchivees;
+    }
 }
