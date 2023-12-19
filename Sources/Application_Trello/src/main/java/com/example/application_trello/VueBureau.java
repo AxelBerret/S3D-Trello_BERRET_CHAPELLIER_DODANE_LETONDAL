64420d27f3cd0f593dd755555fbac139e5d9aeb2
SachaDodane <sachado21@gmail.com>
2023-12-19 08:21:25 +0100

    VueBureau

--- Sources/Application_Trello/src/main/java/com/example/application_trello/VueBureau.java
+++ Sources/Application_Trello/src/main/java/com/example/application_trello/VueBureau.java
@@ -2,8 +2,11 @@ package com.example.application_trello;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -21,30 +24,68 @@ public class VueBureau extends Application {
             leftVBox.getChildren().add(button);
         }
 
-        // Right VBox with HBox, rectangles, and "Gant" button
+        // Right VBox with HBox, rectangles, and "Gantt" button
         VBox rightVBox = new VBox(10); // Added spacing
         rightVBox.setPadding(new Insets(10)); // Added padding
 
         // HBox in the right VBox
         HBox rightHBox = new HBox(10); // Added spacing
 
-        // Create 4 rectangles with specified properties
+        // Create tasks in each column
         for (int i = 0; i < 4; i++) {
-            Rectangle rectangle = new Rectangle(100, 400);
-            rectangle.setArcWidth(10);
-            rectangle.setArcHeight(10);
-            rectangle.setFill(Color.LIGHTGRAY);
+            VBox columnVBox = new VBox(20); // Added spacing
+            columnVBox.setMinWidth(200); // Set minimum width to 200 pixels
+            columnVBox.setMinHeight(500); // Set minimum height to 500 pixels
+            columnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+            columnVBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
 
-            rightHBox.getChildren().add(rectangle);
+            // Label for column title
+            Label columnLabel;
+            switch (i) {
+                case 0:
+                    columnLabel = new Label("A faire");
+                    break;
+                case 1:
+                    columnLabel = new Label("En cours");
+                    break;
+                case 2:
+                    columnLabel = new Label("Terminé");
+                    break;
+                default:
+                    columnLabel = new Label("Unknown");
+            }
+
+            // Center the title horizontally
+            columnLabel.setAlignment(Pos.CENTER);
+
+            // Hyperlink and buttons for each task
+            Hyperlink clickableText = new Hyperlink("Task " + (i + 1));
+            Button button1 = new Button("B1");
+            Button button2 = new Button("B2");
+
+            // HBox for each task
+            HBox taskInColumn = new HBox(10); // Added spacing
+            taskInColumn.setPadding(new Insets(10)); // Added padding
+            taskInColumn.getChildren().addAll(clickableText, button1, button2);
+            columnVBox.getChildren().addAll(columnLabel, taskInColumn);
+            rightHBox.getChildren().add(columnVBox);
+
+            // Add hover effect to the columnVBox
+            columnVBox.setOnMouseEntered(e -> columnVBox.setBackground(new Background(new BackgroundFill(Color.rgb(255, 165, 0, 0.05), CornerRadii.EMPTY, Insets.EMPTY))));
+            columnVBox.setOnMouseExited(e -> columnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY))));
         }
 
-        // Button "Gant" in the right VBox
-        Button gantButton = new Button("Gant");
-        gantButton.setStyle("-fx-font-size: 16; -fx-padding: 10 20;");
-        rightVBox.getChildren().addAll(rightHBox, gantButton);
+        // Styling the "Gantt" button
+        Button ganttButton = new Button("Gantt");
+        ganttButton.setStyle("-fx-font-size: 16; -fx-padding: 10 50; -fx-background-radius: 30 30 30 30; -fx-background-color: blue; -fx-text-fill: white;");
+
+        // Add hover effect to the "Gantt" button
+        ganttButton.setOnMouseEntered(e -> ganttButton.setStyle("-fx-font-size: 16; -fx-padding: 10 50; -fx-background-radius: 30 30 30 30; -fx-background-color: orange; -fx-text-fill: white;"));
+        ganttButton.setOnMouseExited(e -> ganttButton.setStyle("-fx-font-size: 16; -fx-padding: 10 50; -fx-background-radius: 30 30 30 30; -fx-background-color: blue; -fx-text-fill: white;"));
 
         // Main HBox with left and right VBoxes
         HBox mainHBox = new HBox(20, leftVBox, rightVBox); // Added spacing
+        rightVBox.getChildren().addAll(rightHBox, ganttButton);
 
         // Create the scene with the main HBox
         Scene scene = new Scene(mainHBox, 900, 400);
