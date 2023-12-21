51948601a72c19a79dc72bacf0a987e593dc5299
SachaDodane <sachado21@gmail.com>
2023-12-21 08:49:09 +0100

    Maj vueBureau

--- Sources/Application_Trello/src/main/java/com/example/application_trello/VueBureau.java
+++ Sources/Application_Trello/src/main/java/com/example/application_trello/VueBureau.java
@@ -13,6 +13,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class VueBureau extends Application {
+
     @Override
     public void start(Stage stage) {
         // Left VBox with 5 hyperlinks
@@ -38,63 +39,22 @@ public class VueBureau extends Application {
 
         HBox rightHBox = new HBox(10);
 
-        for (int i = 0; i < 4; i++) {
-            VBox columnVBox = new VBox(20);
-            columnVBox.setMinWidth(200);
-            columnVBox.setMinHeight(500);
-            columnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-            columnVBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
-            columnVBox.setAlignment(Pos.TOP_CENTER);
+        for (int i = 0; i < 5; i++) {  // Changement ici pour inclure la cinquième colonne
+            VBox columnVBox;
 
-            Label columnLabel;
-            switch (i) {
-                case 0:
-                    columnLabel = new Label("A faire");
-                    break;
-                case 1:
-                    columnLabel = new Label("En cours");
-                    break;
-                case 2:
-                    columnLabel = new Label("Terminé");
-                    break;
-                default:
-                    columnLabel = new Label("Unknown");
+            if (i < 4) {
+                // Les quatre premières colonnes
+                columnVBox = createTaskColumn(i + 1);
+            } else {
+                // Cinquième colonne spéciale
+                columnVBox = createSpecialColumn();
             }
-            columnLabel.setPadding(new Insets(20));
-            columnLabel.setAlignment(Pos.TOP_CENTER);
 
-            HBox additionalButtonsRow = new HBox(10);
-            additionalButtonsRow.setAlignment(Pos.CENTER);
-            Button newButton1 = createIconButton("trombonne.png");
-            Button newButton2 = createIconButton("croix.png");
-            Button newButton3 = createIconButton("plus.png");
-            additionalButtonsRow.getChildren().addAll(newButton1, newButton2, newButton3);
-
-            Hyperlink clickableText = new Hyperlink("Task " + (i + 1));
-            clickableText.setStyle("-fx-text-fill: black; -fx-underline: none;");
-
-            HBox buttonRow = new HBox(10);
-            buttonRow.setAlignment(Pos.CENTER);
-            Button trombonneButton = createIconButton("trombonne.png");
-            Button croixButton = createIconButton("croix.png");
-            buttonRow.getChildren().addAll(trombonneButton, croixButton);
-
-            HBox taskInColumn = new HBox(10);
-            taskInColumn.setPadding(new Insets(10));
-            taskInColumn.getChildren().addAll(clickableText, buttonRow);
-
-            Separator columnSeparator = new Separator();
-            columnSeparator.setOrientation(Orientation.HORIZONTAL);
-            columnSeparator.setStyle("-fx-background-color: black; -fx-min-height: 2px; -fx-pref-height: 2px; -fx-max-height: 2px;");
-
-            columnVBox.getChildren().addAll(columnLabel, additionalButtonsRow, columnSeparator, taskInColumn);
             rightHBox.getChildren().addAll(columnVBox);
             columnVBox.setOnMouseEntered(e -> columnVBox.setBackground(new Background(new BackgroundFill(Color.rgb(50, 50, 255, 0.05), CornerRadii.EMPTY, Insets.EMPTY))));
             columnVBox.setOnMouseExited(e -> columnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY))));
             HBox.setMargin(columnVBox, new Insets(0, 0, 0, 50));
         }
-
-        
 
         Button ganttButton = new Button("Création du Gantt");
         ganttButton.setStyle("-fx-font-size: 16; -fx-padding: 10 50; -fx-background-radius: 30 30 30 30; -fx-background-color: white; -fx-text-fill: black;");
@@ -103,7 +63,7 @@ public class VueBureau extends Application {
         ganttButton.setOnMouseExited(e -> ganttButton.setStyle("-fx-font-size: 16; -fx-padding: 10 50; -fx-background-radius: 30 30 30 30; -fx-background-color: white; -fx-text-fill: black;"));
 
         HBox mainHBox = new HBox(20, leftVBox, separator, rightVBox);
-        mainHBox.setStyle("-fx-background-color: linear-gradient(to top right, rgba(255,128,0,0.45), rgba(0,128,255,0.45)); -fx-background-radius: 0;");
+        mainHBox.setStyle("-fx-background-color: linear-gradient(to top, rgba(50,0,255,0.45), rgba(200,0,200,0.45)); -fx-background-radius: 0;");
         rightVBox.getChildren().addAll(rightHBox, ganttButton);
 
         Scene scene = new Scene(mainHBox, 900, 400);
@@ -112,6 +72,86 @@ public class VueBureau extends Application {
         stage.setScene(scene);
 
         stage.show();
+    }
+
+    private VBox createTaskColumn(int index) {
+        VBox columnVBox = new VBox(20);
+        columnVBox.setMinWidth(200);
+        columnVBox.setMinHeight(500);
+        columnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        columnVBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+        columnVBox.setAlignment(Pos.TOP_CENTER);
+
+        Label columnLabel;
+        switch (index) {
+            case 1:
+                columnLabel = new Label("A faire");
+                break;
+            case 2:
+                columnLabel = new Label("En cours");
+                break;
+            case 3:
+                columnLabel = new Label("Terminé");
+                break;
+            case 4:
+                columnLabel = new Label("Unknown");
+                break;
+            default:
+                columnLabel = new Label("Unknown");
+        }
+        columnLabel.setPadding(new Insets(20));
+        columnLabel.setAlignment(Pos.TOP_CENTER);
+
+        HBox additionalButtonsRow = new HBox(10);
+        additionalButtonsRow.setAlignment(Pos.CENTER);
+        Button newButton1 = createIconButton("trombonne.png");
+        Button newButton2 = createIconButton("croix.png");
+        Button newButton3 = createIconButton("plus.png");
+        additionalButtonsRow.getChildren().addAll(newButton1, newButton2, newButton3);
+
+        Hyperlink clickableText = new Hyperlink("Task " + index);
+        clickableText.setStyle("-fx-text-fill: black; -fx-underline: none;");
+
+        HBox buttonRow = new HBox(10);
+        buttonRow.setAlignment(Pos.CENTER);
+        Button trombonneButton = createIconButton("trombonne.png");
+        Button croixButton = createIconButton("croix.png");
+        buttonRow.getChildren().addAll(trombonneButton, croixButton);
+
+        HBox taskInColumn = new HBox(10);
+        taskInColumn.setPadding(new Insets(10));
+        taskInColumn.getChildren().addAll(clickableText, buttonRow);
+
+        Separator columnSeparator = new Separator();
+        columnSeparator.setOrientation(Orientation.HORIZONTAL);
+        columnSeparator.setStyle("-fx-background-color: black; -fx-min-height: 2px; -fx-pref-height: 2px; -fx-max-height: 2px;");
+
+        columnVBox.getChildren().addAll(columnLabel, additionalButtonsRow, columnSeparator, taskInColumn);
+
+        return columnVBox;
+    }
+
+    private VBox createSpecialColumn() {
+        VBox specialColumnVBox = new VBox(20);
+        specialColumnVBox.setMinWidth(200);
+        specialColumnVBox.setMaxHeight(30);
+        specialColumnVBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        specialColumnVBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+        specialColumnVBox.setAlignment(Pos.TOP_CENTER);
+
+        Label specialColumnLabel = new Label("Créer une colonne");
+        specialColumnLabel.setPadding(new Insets(15));
+        specialColumnLabel.setAlignment(Pos.CENTER);
+
+        Button plusButton = createIconButton("plus.png");
+
+        HBox specialColumnContent = new HBox(10);
+        specialColumnContent.setAlignment(Pos.CENTER);
+        specialColumnContent.getChildren().addAll(specialColumnLabel, plusButton);
+
+        specialColumnVBox.getChildren().addAll(specialColumnContent);
+
+        return specialColumnVBox;
     }
 
     private Button createIconButton(String imageName) {
