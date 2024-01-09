95c106b0a097454797135e925eb9dc0e974c361f
logan <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-09 11:13:56 +0100

    ajout de la classe TestControlSuppressionTableau

---
+++ Sources/Application_Trello/Tests/TestControlSuppressionTableau.java
@@ -0,0 +1,44 @@
+import com.example.application_trello.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestControlSuppressionTableau {
+    // Attributs
+    public ListeTableaux listeTableaux;
+    public ControlSuppressionTableau controlSuppressionTableau;
+
+    @BeforeEach
+    public void setUp(){
+        // Initialisation des objets nécessaires pour les tests
+        listeTableaux = new ListeTableaux();
+        controlSuppressionTableau = new ControlSuppressionTableau(listeTableaux);
+    }
+
+    /**
+     * test_01
+     * test du constructeur de la classe ControlSuppressionColonne
+     */
+    @Test
+    public void test_01_constructeurControlSuppressionTableau(){
+        // Vérifie si le constructeur est correctement initialisé
+        assertEquals(listeTableaux, controlSuppressionTableau.getListeTab());
+    }
+
+    /**
+     * test_02
+     * test la methode ExtraireNomColonneDeID de la classe ControlSuppressionColonne
+     */
+
+    @Test
+    public void test_02_extraireNomColonneDeID(){
+        // Appeler la méthode extraireNomColonneDeID avec un ID de bouton
+        String nomTab = controlSuppressionTableau.extraireNomTableauDeID("boutonSupprimerTableauTest");
+
+        // Vérifier si le nom de la colonne extrait est correct
+        assertEquals("Test", nomTab);
+    }
+
+}
+
