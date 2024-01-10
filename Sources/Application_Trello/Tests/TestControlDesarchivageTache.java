d84837e2fc84808bf7dc4780ed934e94b5a714f8
logan <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-10 08:35:01 +0100

    Mise a jour TestControlDesarchivageTache

--- Sources/Application_Trello/Tests/TestControlDesarchivageTache.java
+++ Sources/Application_Trello/Tests/TestControlDesarchivageTache.java
@@ -1,4 +1,6 @@
-/**import com.example.application_trello.*;
+import com.example.application_trello.*;
+import com.example.application_trello.Controls.ControlDesarchivageTache;
+import com.example.application_trello.Objects.Tableau;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,27 +10,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * classe TestControlDesarchivageTache
  * test les differentes methodes de la classe ControlDesarchivageTache
  * fait par Logan
-
+*/
 public class TestControlDesarchivageTache {
     // Attributs
     private Tableau tableau;
+    private String nomTache;
+    private String nomCol;
     private ControlDesarchivageTache controlDesarchivageTache;
 
     @BeforeEach
     public void setUp(){
         // Initialisation des objets nécessaires pour les tests
         tableau = new Tableau("Test");
-        controlDesarchivageTache = new ControlDesarchivageTache(tableau);
+        controlDesarchivageTache = new ControlDesarchivageTache(tableau, nomTache, nomCol);
     }
 
     /**
      * test_01
      * test le constructeur de la classe ControlDesarchivageTache
-
+     */
     @Test
     public void test_01_constructeurControlDesarchivageTache(){
         // Vérifier si le tableau est correctement initialisé
         assertEquals(tableau, controlDesarchivageTache.getTab());
     }
 }
- */
