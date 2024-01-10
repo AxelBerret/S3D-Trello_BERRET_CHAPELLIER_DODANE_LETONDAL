cc3ee5e8ab17c82c25414846efcc409b5b7fce6f
logan <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-10 08:34:39 +0100

    Mise a jour TestControlDesarchivageColonne

--- Sources/Application_Trello/Tests/TestControlDesarchivageColonne.java
+++ Sources/Application_Trello/Tests/TestControlDesarchivageColonne.java
@@ -1,4 +1,6 @@
 import com.example.application_trello.*;
+import com.example.application_trello.Controls.ControlDesarchivageColonne;
+import com.example.application_trello.Objects.Tableau;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
