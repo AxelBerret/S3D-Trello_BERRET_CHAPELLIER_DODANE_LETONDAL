3050aa7d5b85aea83b65a2a399a4c445e8ce13b0
logan <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-10 08:32:58 +0100

    Mise a jour TestControlArchivageColonne

--- Sources/Application_Trello/Tests/TestControlArchivageColonne.java
+++ Sources/Application_Trello/Tests/TestControlArchivageColonne.java
@@ -1,4 +1,6 @@
 import com.example.application_trello.*;
+import com.example.application_trello.Controls.ControlArchivageColonne;
+import com.example.application_trello.Objects.Tableau;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
