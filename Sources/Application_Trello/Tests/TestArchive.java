58d6bbdeefa68679afee1cd9646edaf6791ac180
logan <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-10 08:32:17 +0100

    Mise a jour TestArchive

--- Sources/Application_Trello/Tests/TestArchive.java
+++ Sources/Application_Trello/Tests/TestArchive.java
@@ -1,4 +1,8 @@
 import com.example.application_trello.*;
+import com.example.application_trello.Objects.Archive;
+import com.example.application_trello.Objects.Colonne;
+import com.example.application_trello.Objects.Tache;
+import com.example.application_trello.Objects.TacheSimple;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
